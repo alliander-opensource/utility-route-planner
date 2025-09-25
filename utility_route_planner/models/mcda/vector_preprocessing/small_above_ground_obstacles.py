976f4d26a3a4d985e9b9a5d2baca137c76754b3c
Jelmar Versleijen <jelmar.versleijen@alliander.com>
2025-09-25 18:39:53 +0200

    Fix mcda bug when there are only 1 type of obstacles as input gdfs
    Signed-off-by: Jelmar Versleijen <jelmar.versleijen@alliander.com>

--- utility_route_planner/models/mcda/vector_preprocessing/small_above_ground_obstacles.py
+++ utility_route_planner/models/mcda/vector_preprocessing/small_above_ground_obstacles.py
@@ -29,42 +29,49 @@ class SmallAboveGroundObstacles(VectorPreprocessorBase):
 
     @staticmethod
     def _set_suitability_values(input_gdf: list[gpd.GeoDataFrame], weight_values: dict) -> gpd.GeoDataFrame:
-        # bgt_scheiding has different fields which we process first (first two gdfs).
-        logger.info("Merging bgt scheiding tables.")
+        logger.info("Processing above ground obstacle tables.")
         # identify bgt-type geodataframes, process them first here. do not index
         bgt_scheiding = []
         bgt_others = []
         for gdf in input_gdf:
+            # bgt_scheiding has different fields which we process first (first two gdfs).
             if "bgt-type" in gdf.columns:
                 bgt_scheiding.append(gdf)
             else:
                 bgt_others.append(gdf)
-        gdf_bgt_scheiding = pd.concat(bgt_scheiding)
-        validate_values_to_reclassify(gdf_bgt_scheiding["bgt-type"].unique().tolist(), weight_values)
-        logger.info("Setting suitability values.")
-        # Function is always filled in.
-        gdf_bgt_scheiding["sv_1"] = gdf_bgt_scheiding["bgt-type"]
-        gdf_bgt_scheiding["sv_1"] = gdf_bgt_scheiding["sv_1"].case_when(
-            [(gdf_bgt_scheiding["sv_1"].eq(i), weight_values[i]) for i in weight_values]
-        )
-        gdf_bgt_scheiding = gdf_bgt_scheiding[gdf_bgt_scheiding["bgt-type"] != "niet-bgt"]
-        gdf_bgt_scheiding["suitability_value"] = gdf_bgt_scheiding["sv_1"]
 
-        logger.info("Merging remaining obstacles.")
-        gdf_remaining_obstacles = pd.concat(bgt_others)
-        gdf_remaining_obstacles = gdf_remaining_obstacles.dropna(subset=["plus-type", "function"], how="all")
-        gdf_remaining_obstacles = gdf_remaining_obstacles[
-            ~(gdf_remaining_obstacles["function"].isin(["niet-bgt"]) & gdf_remaining_obstacles["plus-type"].isna())
-        ]
-        gdf_remaining_obstacles = gdf_remaining_obstacles[gdf_remaining_obstacles["function"] != "waardeOnbekend"]
-        validate_values_to_reclassify(gdf_remaining_obstacles["plus-type"].unique().tolist(), weight_values)
-        # plus-type is not always filled in.
-        gdf_remaining_obstacles["sv_1"] = gdf_remaining_obstacles["plus-type"]
-        gdf_remaining_obstacles["sv_1"] = gdf_remaining_obstacles["sv_1"].case_when(
-            [(gdf_remaining_obstacles["sv_1"].eq(i), weight_values[i]) for i in weight_values]
-        )
-        gdf_remaining_obstacles = gdf_remaining_obstacles[gdf_remaining_obstacles["plus-type"] != "waardeOnbekend"]
-        gdf_remaining_obstacles["suitability_value"] = gdf_remaining_obstacles["sv_1"]
+        gdf_bgt_scheiding = pd.DataFrame()
+        gdf_remaining_obstacles = pd.DataFrame()
+
+        if bgt_scheiding:
+            logger.info("Processing bgt scheiding.")
+            gdf_bgt_scheiding = pd.concat(bgt_scheiding)
+            validate_values_to_reclassify(gdf_bgt_scheiding["bgt-type"].unique().tolist(), weight_values)
+            logger.info("Setting suitability values.")
+            # Function is always filled in.
+            gdf_bgt_scheiding["sv_1"] = gdf_bgt_scheiding["bgt-type"]
+            gdf_bgt_scheiding["sv_1"] = gdf_bgt_scheiding["sv_1"].case_when(
+                [(gdf_bgt_scheiding["sv_1"].eq(i), weight_values[i]) for i in weight_values]
+            )
+            gdf_bgt_scheiding = gdf_bgt_scheiding[gdf_bgt_scheiding["bgt-type"] != "niet-bgt"]
+            gdf_bgt_scheiding["suitability_value"] = gdf_bgt_scheiding["sv_1"]
+
+        if bgt_others:
+            logger.info("Processing remaining obstacles.")
+            gdf_remaining_obstacles = pd.concat(bgt_others)
+            gdf_remaining_obstacles = gdf_remaining_obstacles.dropna(subset=["plus-type", "function"], how="all")
+            gdf_remaining_obstacles = gdf_remaining_obstacles[
+                ~(gdf_remaining_obstacles["function"].isin(["niet-bgt"]) & gdf_remaining_obstacles["plus-type"].isna())
+            ]
+            gdf_remaining_obstacles = gdf_remaining_obstacles[gdf_remaining_obstacles["function"] != "waardeOnbekend"]
+            validate_values_to_reclassify(gdf_remaining_obstacles["plus-type"].unique().tolist(), weight_values)
+            # plus-type is not always filled in.
+            gdf_remaining_obstacles["sv_1"] = gdf_remaining_obstacles["plus-type"]
+            gdf_remaining_obstacles["sv_1"] = gdf_remaining_obstacles["sv_1"].case_when(
+                [(gdf_remaining_obstacles["sv_1"].eq(i), weight_values[i]) for i in weight_values]
+            )
+            gdf_remaining_obstacles = gdf_remaining_obstacles[gdf_remaining_obstacles["plus-type"] != "waardeOnbekend"]
+            gdf_remaining_obstacles["suitability_value"] = gdf_remaining_obstacles["sv_1"]
 
         # Merge dfs
         gdf_merged = pd.concat([gdf_bgt_scheiding, gdf_remaining_obstacles])
