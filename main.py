093bb9b5771fe3d34e84e13f0c1674c8d06fb4d4
Jelmar Versleijen <jelmar.versleijen@alliander.com>
2025-09-26 12:55:08 +0200

    Add the benchmark routes as separate dataclass for reusability
    Signed-off-by: Jelmar Versleijen <jelmar.versleijen@alliander.com>

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # SPDX-FileCopyrightText: Contributors to the utility-route-project and Alliander N.V.
 #
 # SPDX-License-Identifier: Apache-2.0
-
 import pathlib
 import time
 
 import shapely
 import structlog
 
+from utility_route_planner.models.benchmark_routes import BenchmarkRouteCollection
 from utility_route_planner.models.lcpa.lcpa_engine import LcpaUtilityRouteEngine
 from settings import Config
 from utility_route_planner.models.mcda.mcda_engine import McdaCostSurfaceEngine
@@ -27,6 +27,7 @@ def run_mcda_lcpa(
     human_designed_route: shapely.LineString,
     raster_name_prefix: str,
     compute_rasters_in_parallel: bool,
+    compute_metrics: bool = True,
 ):
     reset_geopackage(Config.PATH_GEOPACKAGE_MCDA_OUTPUT, truncate=False)
 
@@ -49,67 +50,32 @@ def run_mcda_lcpa(
     )
 
     logger.info(f"Route CPU time: {(time.process_time_ns() - start_cpu_time) / 1e9:.2f} seconds.")
-    route_evaluation_metrics = RouteEvaluationMetrics(
-        lcpa_engine.lcpa_result, path_suitability_raster, human_designed_route, project_area_geometry
-    )
-    route_evaluation_metrics.get_route_evaluation_metrics()
+    if compute_metrics:
+        route_evaluation_metrics = RouteEvaluationMetrics(
+            lcpa_engine.lcpa_result, path_suitability_raster, human_designed_route, project_area_geometry
+        )
+        route_evaluation_metrics.get_route_evaluation_metrics()
 
 
 if __name__ == "__main__":
-    cases = [
-        (
-            Config.PATH_GEOPACKAGE_CASE_01,
-            Config.LAYER_NAME_PROJECT_AREA_CASE_01,
-            Config.LAYER_NAME_HUMAN_DESIGNED_ROUTE_CASE_01,
-            "route_1_",
-            [],
-        ),
-        (
-            Config.PATH_GEOPACKAGE_CASE_02,
-            Config.LAYER_NAME_PROJECT_AREA_CASE_02,
-            Config.LAYER_NAME_HUMAN_DESIGNED_ROUTE_CASE_02,
-            "route_2_",
-            [],
-        ),
-        (
-            Config.PATH_GEOPACKAGE_CASE_03,
-            Config.LAYER_NAME_PROJECT_AREA_CASE_03,
-            Config.LAYER_NAME_HUMAN_DESIGNED_ROUTE_CASE_03,
-            "route_3_",
-            [],
-        ),
-        (
-            Config.PATH_GEOPACKAGE_CASE_04,
-            Config.LAYER_NAME_PROJECT_AREA_CASE_04,
-            Config.LAYER_NAME_HUMAN_DESIGNED_ROUTE_CASE_04,
-            "route_4_",
-            [],
-        ),
-        (
-            Config.PATH_GEOPACKAGE_CASE_05,
-            Config.LAYER_NAME_PROJECT_AREA_CASE_05,
-            Config.LAYER_NAME_HUMAN_DESIGNED_ROUTE_CASE_05,
-            "route_5_",
-            [[121462.8, 487153.4]],
-        ),
-    ]
-
     reset_geopackage(Config.PATH_GEOPACKAGE_LCPA_OUTPUT, truncate=True)
+    route_collection = BenchmarkRouteCollection()
 
-    cases_to_run = [0, 1, 2, 3, 4]  # 0/1/2/3/4
-    for case in cases_to_run:
-        geopackage, layer_project_area, human_designed_route_name, raster_name_prefix, stops = cases[case]
-        human_designed_route = gpd.read_file(geopackage, layer=human_designed_route_name).iloc[0].geometry
+    cases_to_run = [5]  # 1 to 5, or [] for all
+    for case in route_collection.get_routes(cases_to_run):
+        human_designed_route = (
+            gpd.read_file(case.path_geopackage, layer=case.layer_name_human_designed_route).iloc[0].geometry
+        )
         route_stops = get_first_last_point_from_linestring(human_designed_route)
-        if stops:
-            route_stops = list(route_stops)[:1] + [shapely.Point(i) for i in stops] + list(route_stops)[1:]  # type: ignore
+        if case.stops:
+            route_stops = list(route_stops)[:1] + [shapely.Point(i) for i in case.stops] + list(route_stops)[1:]  # type: ignore
 
         run_mcda_lcpa(
             "preset_benchmark_raw",
-            geopackage,
-            gpd.read_file(geopackage, layer=layer_project_area).iloc[0].geometry,
+            case.path_geopackage,
+            gpd.read_file(case.path_geopackage, layer=case.layer_name_project_area).iloc[0].geometry,
             route_stops,
             human_designed_route,
-            raster_name_prefix,
+            case.raster_name_prefix,
             compute_rasters_in_parallel=True,
         )
