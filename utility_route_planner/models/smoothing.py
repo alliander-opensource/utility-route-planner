117981712017899c3ea6843e9432a669020caf28
Jelmar Versleijen <jelmar.versleijen@alliander.com>
2025-12-24 18:45:44 +0100

    Add inspiration for smoothing
    Signed-off-by: Jelmar Versleijen <jelmar.versleijen@alliander.com>

---
+++ utility_route_planner/models/smoothing.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: Contributors to the utility-route-project and Alliander N.V.
+#
+# SPDX-License-Identifier: Apache-2.0
+import shapely
+
+
+def smooth(route: shapely.LineString) -> shapely.LineString:
+    # inspiration: https://www.sciencedirect.com/science/article/pii/S3050520825000211#sec3
+    pass
