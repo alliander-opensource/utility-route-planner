d678c34607521a823af16b3fb21f772ae37e2ab8
Djesse Dirckx <djesse.dirckx@alliander.com>
2026-02-19 15:01:59 +0100

    Only store block edges to save memory
    Signed-off-by: Djesse Dirckx <djesse.dirckx@alliander.com>

--- utility_route_planner/models/multilayer_network/hexagon_graph/hexagon_graph_builder.py
+++ utility_route_planner/models/multilayer_network/hexagon_graph/hexagon_graph_builder.py
@@ -1,7 +1,11 @@
 # SPDX-FileCopyrightText: Contributors to the utility-route-project and Alliander N.V.
 #
 # SPDX-License-Identifier: Apache-2.0
+from dataclasses import asdict
+
 import geopandas as gpd
+import pandas as pd
+import polars as pl
 import rustworkx as rx
 import shapely
 import structlog
@@ -47,32 +51,69 @@ class HexagonGraphBuilder:
         hexagon_edge_generator = HexagonEdgeGenerator()
         previous_row: dict[tuple[int, int], TempNode] = {}
         current_row: dict[tuple[int, int], TempNode] = {}
+
+        node_ids: list[int] = []
+        node_suitability_values: list[int] = []
+        node_x_coordinates: list[float] = []
+        node_y_coordinates: list[float] = []
+        node_is_osm: list[bool] = []
+
         for block, final_column in grid_constructor.construct_grid(self.project_area):
             suitability_values = block.loc[:, "suitability_value"].values
-            node_ids = self.graph.add_nodes_from(suitability_values)
-            block.index = node_ids
+            block_node_ids = self.graph.add_nodes_from(suitability_values)
+            block.index = block_node_ids
 
-            block_coordinates: dict[tuple[int, int], TempNode] = {
-                (node.axial_q, node.axial_r): TempNode(node.Index, node.suitability_value)
-                for node in block.itertuples()
+            block_coordinates: dict[tuple[int, int], TempNode] = {}
+            for node in block.itertuples():
+                node_ids.append(node.Index)
+                node_suitability_values.append(node.suitability_value)
+                node_x_coordinates.append(node.x)
+                node_y_coordinates.append(node.y)
+                node_is_osm.append(True)
+                block_coordinates[(node.axial_q, node.axial_r)] = TempNode(node.Index, node.suitability_value)
+
+            blocks_to_check = previous_row | current_row | block_coordinates
+            blocks_to_check_df = pd.DataFrame(
+                [{"axial_q": q, "axial_r": r, **asdict(v)} for (q, r), v in blocks_to_check.items()]
+            )
+            blocks_to_check_df = blocks_to_check_df.set_index(keys=["node_id"])
+
+            for edges in hexagon_edge_generator.generate(block, blocks_to_check_df):
+                self.graph.add_edges_from(edges)
+
+            # Max r represents the bottom of the block
+            max_r = max(r for _, r in block_coordinates)
+
+            # Max q represents the bottom of the block
+            max_q = max(q for q, _ in block_coordinates)
+
+            # Get all coordinates on the edges of the blocks
+            edge_coordinates = {
+                (q, r): value for (q, r), value in block_coordinates.items() if q == max_q or r == max_r
             }
 
             if not final_column:
-                current_row.update(block_coordinates)
+                current_row.update(edge_coordinates)
             else:
                 previous_row = current_row
-                current_row = block_coordinates
+                current_row = edge_coordinates
 
-            blocks_to_check = previous_row | current_row
-            for edges in hexagon_edge_generator.generate(block, blocks_to_check):
-                self.graph.add_edges_from(edges)
-
-        degrees = [self.graph.degree(node) for node in self.graph.nodes()]
-        logger.info(
-            f"Max degree: {max(degrees)}, Min degree: {min(degrees)}, avg: {sum(degrees) / self.graph.num_nodes()}"
+        nodes_df = pl.DataFrame(
+            {
+                "node_id": node_ids,
+                "suitability_value": node_suitability_values,
+                "x": node_x_coordinates,
+                "y": node_y_coordinates,
+                "is_osm": node_is_osm,
+            },
+            schema={
+                "node_id": pl.Int32,
+                "suitability_value": pl.Float32,
+                "x": pl.Float64,
+                "y": pl.Float64,
+                "is_osm": pl.Boolean,
+            },
         )
-        logger.info(
-            f"Graph has {self.graph.num_nodes()} nodes & {self.graph.num_edges()} edges for hexagon_size {self.hexagon_size}"
-        )
+        logger.info(f"Nodes df estimated size: {nodes_df.estimated_size(unit='gb')}gb")
 
         return self.graph
