b8352e864ef2564c4b4a0c962b2c1359da833bb7
Djesse Dirckx <djesse.dirckx@alliander.com>
2026-04-02 10:04:03 +0200

    Test edge generator without previous blocks
    Signed-off-by: Djesse Dirckx <djesse.dirckx@alliander.com>

--- tests/unit/multilayer_network/hexagon_graph/hexagon_edge_generator_test.py
+++ tests/unit/multilayer_network/hexagon_graph/hexagon_edge_generator_test.py
@@ -2,158 +2,117 @@
 #  #
 #  SPDX-License-Identifier: Apache-2.0
 import geopandas as gpd
-import geopandas.testing
-import pandas as pd
+import numpy as np
+import polars as pl
+from polars.testing import assert_frame_equal
 import pytest
 import shapely
 
 from settings import Config
 from utility_route_planner.models.multilayer_network.hexagon_graph.hexagon_edge_generator import HexagonEdgeGenerator
-from utility_route_planner.util.write import write_results_to_geopackage
+from utility_route_planner.util.write import reset_geopackage, write_results_to_geopackage
 
 
 class TestHexagonEdgeGenerator:
     @pytest.fixture()
-    def hexagonal_grid(self) -> gpd.GeoDataFrame:
-        return gpd.GeoDataFrame(
+    def hexagonal_grid(self) -> pl.DataFrame:
+        """
+        Hexagonal grid generated with hexagonsize=4
+
+        """
+        return pl.DataFrame(
             data=[
-                [-233233, 637464, 10, 174924.804, 451067.279],
-                [-233233, 637465, 10, 174924.804, 451068.145],
-                [-233236, 637465, 10, 174927.054, 451066.846],
-                [-233234, 637464, 10, 174925.554, 451066.846],
-                [-233237, 637466, 20, 174927.804, 451067.279],
-                [-233235, 637465, 20, 174926.304, 451067.279],
-                [-233234, 637465, 20, 174925.554, 451067.712],
-                [-233236, 637466, 20, 174927.054, 451067.712],
-                [-233237, 637467, 30, 174927.804, 451068.145],
-                [-233235, 637466, 30, 174926.304, 451068.145],
-                [-233234, 637466, 30, 174925.554, 451068.578],
-                [-233236, 637467, 30, 174927.054, 451068.578],
+                [0, -29159, 79677, 126, 174954.55, 451007.03],
+                [1, -29160, 79678, 76, 174960.55, 451010.50],
+                [2, -29161, 79678, 9, 174966.55, 451007.03],
+                [3, -29160, 79677, 76, 174960.55, 451003.56],
+                [4, -29159, 79676, 76, 174954.55, 451000.09],
+                [5, -29160, 79676, 86, 174960.55, 450996.63],
+                [6, -29161, 79677, 9, 174966.55, 451000.09],
             ],
-            columns=["axial_q", "axial_r", "suitability_value", "x", "y"],
+            schema={
+                "node_id": pl.Int32,
+                "q": pl.Int32,
+                "r": pl.Int32,
+                "suitability_value": pl.Int16,
+                "x": pl.Float32,
+                "y": pl.Float32,
+            },
         )
 
-    def test_generate_edges(self, hexagonal_grid: gpd.GeoDataFrame, debug: bool = False):
+    def test_generate_edges_without_previous_blocks(self, hexagonal_grid: pl.DataFrame, debug: bool = False):
         """
+        Test that verifies that all edges are present, have correct edge weights and length when generating edges
+        for a block without having previous edges. This means only edges within the block itself are expected.
+
         This test can be understood most easily by setting debug=True and inspecting the results in QGis.
         """
-        generator = HexagonEdgeGenerator(hexagonal_grid)
-        vertical_edges, left_edges, right_edges = [*generator.generate()]
+        generator = HexagonEdgeGenerator()
 
-        self.verify_vertical_edges(vertical_edges)
-        self.verify_left_edges(left_edges)
-        self.verify_right_edges(right_edges)
+        empty_previous_edges = pl.DataFrame(
+            schema={"node_id": pl.Int32, "suitability_value": pl.Int16, "q": pl.Int32, "r": pl.Int32}
+        )
+        nodes_to_check = pl.concat(
+            [hexagonal_grid.select("node_id", "suitability_value", "q", "r"), empty_previous_edges]
+        )
+        result_edges = generator.generate(hexagonal_grid, nodes_to_check)
+
+        expected_edges = pl.DataFrame(
+            data=[
+                [5, 6, 95],
+                [3, 6, 85],
+                [4, 0, 202],
+                [0, 3, 202],
+                [3, 2, 85],
+                [4, 5, 162],
+                [3, 1, 152],
+                [1, 2, 85],
+                [6, 2, 18],
+                [5, 3, 162],
+                [4, 3, 152],
+                [0, 1, 202],
+            ],
+            schema={"source_node": pl.Int32, "target_node": pl.Int32, "weight": pl.Int16},
+        )
+        assert_frame_equal(expected_edges, result_edges, check_row_order=False)
+
+        hexagon_points, edges_linestrings = self.convert_nodes_and_edges_to_gdfs(hexagonal_grid, result_edges)
+        assert all([length == pytest.approx(6.93, abs=0.01) for length in edges_linestrings.length])
 
         if debug:
-            hexagon_points = gpd.GeoDataFrame(
-                geometry=gpd.points_from_xy(hexagonal_grid["x"], hexagonal_grid["y"]), crs=Config.CRS
+            reset_geopackage(Config.PATH_GEOPACKAGE_VECTOR_GRAPH_OUTPUT)
+
+            write_results_to_geopackage(
+                Config.PATH_GEOPACKAGE_VECTOR_GRAPH_OUTPUT,
+                hexagon_points,
+                "pytest_graph_nodes_no_previous_blocks",
+                overwrite=True,
             )
             write_results_to_geopackage(
-                Config.PATH_GEOPACKAGE_VECTOR_GRAPH_OUTPUT, hexagon_points, "graph_test_nodes", overwrite=True
-            )
-            write_results_to_geopackage(
-                Config.PATH_GEOPACKAGE_VECTOR_GRAPH_OUTPUT, vertical_edges, "graph_test_vertical_edges", overwrite=True
-            )
-            write_results_to_geopackage(
-                Config.PATH_GEOPACKAGE_VECTOR_GRAPH_OUTPUT, left_edges, "graph_test_left_edges", overwrite=True
-            )
-            write_results_to_geopackage(
-                Config.PATH_GEOPACKAGE_VECTOR_GRAPH_OUTPUT, right_edges, "graph_test_right_edges", overwrite=True
+                Config.PATH_GEOPACKAGE_VECTOR_GRAPH_OUTPUT,
+                edges_linestrings,
+                "pytest_graph_edges_no_previous_blocks",
+                overwrite=True,
             )
 
-    def verify_vertical_edges(self, vertical_edges_result: gpd.GeoDataFrame):
-        expected_edges = gpd.GeoDataFrame(
-            data=[
-                [0, 1, 10.0],
-                [2, 7, 15.0],
-                [3, 6, 15.0],
-                [4, 8, 25.0],
-                [5, 9, 25.0],
-                [6, 10, 25.0],
-                [7, 11, 25.0],
-            ],
-            geometry=[
-                shapely.LineString([shapely.Point(174924.804, 451067.279), shapely.Point(174924.804, 451068.145)]),
-                shapely.LineString([shapely.Point(174927.054, 451066.846), shapely.Point(174927.054, 451067.712)]),
-                shapely.LineString([shapely.Point(174925.554, 451066.846), shapely.Point(174925.554, 451067.712)]),
-                shapely.LineString([shapely.Point(174927.804, 451067.279), shapely.Point(174927.804, 451068.145)]),
-                shapely.LineString([shapely.Point(174926.304, 451067.279), shapely.Point(174926.304, 451068.145)]),
-                shapely.LineString([shapely.Point(174925.554, 451067.712), shapely.Point(174925.554, 451068.578)]),
-                shapely.LineString([shapely.Point(174927.054, 451067.712), shapely.Point(174927.054, 451068.578)]),
-            ],
-            columns=["node_id_source", "node_id_target", "weight"],
+    @staticmethod
+    def convert_nodes_and_edges_to_gdfs(
+        grid: pl.DataFrame, edges: pl.DataFrame
+    ) -> tuple[gpd.GeoDataFrame, gpd.GeoDataFrame]:
+        hexagon_points = gpd.GeoDataFrame(
+            data=grid.select("node_id", "suitability_value"),
+            geometry=gpd.points_from_xy(grid["x"], grid["y"]),
+            columns=["node_id", "suitability_value"],
+            crs=Config.CRS,
+        ).set_index("node_id")
+        source_points = hexagon_points.loc[edges["source_node"], "geometry"]
+        target_points = hexagon_points.loc[edges["target_node"], "geometry"]
+        edges_linestrings = gpd.GeoDataFrame(
+            data=edges["weight"],
+            geometry=shapely.linestrings(
+                np.stack([source_points.get_coordinates().values, target_points.get_coordinates().values], axis=1)
+            ),
+            columns=["weight"],
             crs=Config.CRS,
         )
-        # Edge lengths must be tested separately, as geopandas does not allow float tolerance
-        expected_lengths = pd.Series([0.866, 0.866, 0.866, 0.866, 0.866, 0.866, 0.866], name="length")
-
-        gpd.testing.assert_geodataframe_equal(
-            expected_edges, vertical_edges_result[["node_id_source", "node_id_target", "weight", "geometry"]]
-        )
-        pd.testing.assert_series_equal(expected_lengths, vertical_edges_result["length"], atol=0.01)
-
-    def verify_left_edges(self, left_edges_result: gpd.GeoDataFrame):
-        expected_edges = gpd.GeoDataFrame(
-            data=[
-                [0, 3, 10.0],
-                [1, 6, 15.0],
-                [5, 2, 15.0],
-                [6, 5, 20.0],
-                [7, 4, 20.0],
-                [9, 7, 25.0],
-                [10, 9, 30.0],
-                [11, 8, 30.0],
-            ],
-            geometry=[
-                shapely.LineString([shapely.Point(174924.804, 451067.279), shapely.Point(174925.554, 451066.846)]),
-                shapely.LineString([shapely.Point(174924.804, 451068.145), shapely.Point(174925.554, 451067.712)]),
-                shapely.LineString([shapely.Point(174926.304, 451067.279), shapely.Point(174927.054, 451066.846)]),
-                shapely.LineString([shapely.Point(174925.554, 451067.712), shapely.Point(174926.304, 451067.279)]),
-                shapely.LineString([shapely.Point(174927.054, 451067.712), shapely.Point(174927.804, 451067.279)]),
-                shapely.LineString([shapely.Point(174926.304, 451068.145), shapely.Point(174927.054, 451067.712)]),
-                shapely.LineString([shapely.Point(174925.554, 451068.578), shapely.Point(174926.304, 451068.145)]),
-                shapely.LineString([shapely.Point(174927.054, 451068.578), shapely.Point(174927.804, 451068.145)]),
-            ],
-            columns=["node_id_source", "node_id_target", "weight"],
-            crs=Config.CRS,
-        )
-        # Edge lengths must be tested separately, as geopandas does not allow float tolerance
-        expected_lengths = pd.Series([0.866, 0.866, 0.866, 0.866, 0.866, 0.866, 0.866, 0.866], name="length")
-
-        gpd.testing.assert_geodataframe_equal(
-            expected_edges, left_edges_result[["node_id_source", "node_id_target", "weight", "geometry"]]
-        )
-        pd.testing.assert_series_equal(expected_lengths, left_edges_result["length"], atol=0.01)
-
-    def verify_right_edges(self, right_edges_result: gpd.GeoDataFrame):
-        expected_edges = gpd.GeoDataFrame(
-            data=[
-                [4, 2, 15.0],
-                [5, 3, 15.0],
-                [6, 0, 15.0],
-                [7, 5, 20.0],
-                [8, 7, 25.0],
-                [9, 6, 25.0],
-                [10, 1, 20.0],
-                [11, 9, 30.0],
-            ],
-            geometry=[
-                shapely.LineString([shapely.Point(174927.804, 451067.279), shapely.Point(174927.054, 451066.846)]),
-                shapely.LineString([shapely.Point(174926.304, 451067.279), shapely.Point(174925.554, 451066.846)]),
-                shapely.LineString([shapely.Point(174925.554, 451067.712), shapely.Point(174924.804, 451067.279)]),
-                shapely.LineString([shapely.Point(174927.054, 451067.712), shapely.Point(174926.304, 451067.279)]),
-                shapely.LineString([shapely.Point(174927.804, 451068.145), shapely.Point(174927.054, 451067.712)]),
-                shapely.LineString([shapely.Point(174926.304, 451068.145), shapely.Point(174925.554, 451067.712)]),
-                shapely.LineString([shapely.Point(174925.554, 451068.578), shapely.Point(174924.804, 451068.145)]),
-                shapely.LineString([shapely.Point(174927.054, 451068.578), shapely.Point(174926.304, 451068.145)]),
-            ],
-            columns=["node_id_source", "node_id_target", "weight"],
-            crs=Config.CRS,
-        )
-        # Edge lengths must be tested separately, as geopandas does not allow float tolerance
-        expected_lengths = pd.Series([0.866, 0.866, 0.866, 0.866, 0.866, 0.866, 0.866, 0.866], name="length")
-
-        gpd.testing.assert_geodataframe_equal(
-            expected_edges, right_edges_result[["node_id_source", "node_id_target", "weight", "geometry"]]
-        )
-        pd.testing.assert_series_equal(expected_lengths, right_edges_result["length"], atol=0.01)
+        return hexagon_points, edges_linestrings
