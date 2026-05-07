866f094df730246e6c33fd0ef6e20e700ce42660
Djesse Dirckx <djesse.dirckx@alliander.com>
2026-05-07 15:50:20 +0200

    Implemented path dissimilarity check. Working for small project area graphs
    Signed-off-by: Djesse Dirckx <djesse.dirckx@alliander.com>

--- tests/integration/multilayer_network/k_shortest_path_test.py
+++ tests/integration/multilayer_network/k_shortest_path_test.py
@@ -1,10 +1,10 @@
 # SPDX-FileCopyrightText: Contributors to the utility-route-project and Alliander N.V.
 #
 # SPDX-License-Identifier: Apache-2.0
-import geopandas as gpd
 import pytest
 import networkx as nx
 import rustworkx as rx
+from shapely import Polygon, Point
 
 from settings import Config
 from utility_route_planner.models.multilayer_network.k_shortest_path_route_planner import KShortestPathRoutePlanner
@@ -15,29 +15,41 @@ from utility_route_planner.util.write import write_results_to_geopackage
 
 class TestKShortestPaths:
     out = Config.PATH_GEOPACKAGE_MULTILAYER_NETWORK_OUTPUT
-    debug: bool = False
+    debug: bool = True
+
+    @pytest.fixture()
+    def project_area(self) -> Polygon:
+        return Polygon(
+            [
+                Point(174730.23, 451038.86),
+                Point(175171.37, 451080.10),
+                Point(175276.86, 450903.64),
+                Point(174674.60, 450792.40),
+                Point(174730.23, 451038.86),
+            ]
+        )
 
     @pytest.fixture
     def route_planner(self) -> KShortestPathRoutePlanner:
-        return KShortestPathRoutePlanner()
+        return KShortestPathRoutePlanner(similarity_threshold=0.5)
 
     @pytest.fixture()
-    def preprocessed_graph(self, load_osm_graph_pickle: nx.MultiDiGraph) -> rx.PyGraph:
-        project_area = gpd.read_file(
-            Config.PYTEST_PATH_GEOPACKAGE_MCDA, layer=Config.PYTEST_LAYER_NAME_PROJECT_AREA
-        ).geometry.iloc[0]
-
+    def preprocessed_graph(self, load_osm_graph_pickle: nx.MultiDiGraph, project_area: Polygon) -> rx.PyGraph:
         osm_graph_preprocessor = OSMGraphPreprocessor(load_osm_graph_pickle, project_area)
         return osm_graph_preprocessor.preprocess_graph()
 
     def test_shortest_paths(self, route_planner: KShortestPathRoutePlanner, preprocessed_graph: rx.PyGraph):
         k = 3
-        source_node, target_node = 328, 35
+        source_node, target_node = 29, 0
 
-        route = route_planner.find_k_routes(preprocessed_graph, source=source_node, target=target_node, k=k)
+        routes = route_planner.find_k_routes(preprocessed_graph, source=source_node, target=target_node, k=k)
 
         if self.debug:
             nodes, edges = osm_graph_to_gdfs(preprocessed_graph)
             write_results_to_geopackage(self.out, nodes, "pytest_k_shortest_paths_osm_graph_nodes", overwrite=True)
             write_results_to_geopackage(self.out, edges, "pytest_k_shortest_paths_osm_graph_edges", overwrite=True)
-            write_results_to_geopackage(self.out, route, "pytest_k_shortest_paths_osm_graph_route", overwrite=True)
+
+            for i, route in enumerate(routes):
+                write_results_to_geopackage(
+                    self.out, route, f"pytest_k_shortest_paths_osm_graph_route_{i + 1}", overwrite=True
+                )
--- utility_route_planner/models/multilayer_network/k_shortest_path_route_planner.py
+++ utility_route_planner/models/multilayer_network/k_shortest_path_route_planner.py
@@ -18,8 +18,10 @@ class KShortestPathRoutePlanner:
     def __init__(self, similarity_threshold: float):
         self.similarity_threshold = similarity_threshold
 
-    def find_k_routes(self, graph: rx.PyGraph, source: int, target: int, k: int):
-        self.onepass(graph, source, target, k)
+    def find_k_routes(self, graph: rx.PyGraph, source: int, target: int, k: int) -> list[LineString]:
+        result_routes = self.onepass(graph, source, target, k)
+        result_linestrings = [self._convert_osm_route_to_linestring(graph, route) for route in result_routes]
+        return result_linestrings
 
     def onepass(self, graph: rx.PyGraph, source: int, target: int, k: int):
         initial_route = rx.dijkstra_shortest_paths(graph, source, target, weight_fn=lambda x: x.length)[target]
@@ -30,17 +32,17 @@ class KShortestPathRoutePlanner:
         heapq.heappush(candidate_route_queue, (0.0, source, [source]))
 
         while candidate_route_queue and len(result_routes) < k:
-            cost, node_n, candidate_path = heapq.heappop(candidate_route_queue)
+            candidate_path_cost, node_n, candidate_path = heapq.heappop(candidate_route_queue)
 
             # Target node is reached. As similarity is already checked when adding the path to the queue, this serves
             # as a valid path and can therefore be added to the results
             if node_n == target:
                 result_routes.append(candidate_path)
 
-                # Prune queue by removing candidate paths that do not pass the sim check
+                # Prune queue by removing queue paths that are similar to
                 pruned_queue = []
                 for queue_path_cost, queue_node, queue_path in candidate_route_queue:
-                    if not self.path_is_similar(queue_path, result_routes):
+                    if all(self.path_not_similar(graph, queue_path, result_route) for result_route in result_routes):
                         pruned_queue.append((queue_path_cost, queue_node, queue_path))
                 # Update queue with pruned queue
                 heapq.heapify(pruned_queue)
@@ -54,16 +56,36 @@ class KShortestPathRoutePlanner:
 
                     neighbour_edge_data = graph.get_edge_data(node_n, neighbour_node)
                     potential_candidate_path = candidate_path + [neighbour_node]
-                    potential_candidate_path_cost = neighbour_edge_data.geometry.length
 
-                    if not self.path_is_similar(potential_candidate_path, result_routes):
+                    potential_candidate_path_cost = candidate_path_cost + neighbour_edge_data.length
+
+                    # Only add the candidate path to the queue if is dissimilar to the already found routes
+                    if all(
+                        self.path_not_similar(graph, potential_candidate_path, result_route)
+                        for result_route in result_routes
+                    ):
                         heapq.heappush(
                             candidate_route_queue,
                             (potential_candidate_path_cost, neighbour_node, potential_candidate_path),
                         )
+        return result_routes
 
-    def path_is_similar(self, queue_path: list[int], result_routes: list[list[int]]) -> bool:
-        return True
+    def path_not_similar(self, graph: rx.PyGraph, candidate_route: list[int], result_route: list[int]) -> bool:
+        # TODO: move everything to edge ids?
+        # TODO: candidate edge do only have to be retrieved once? Result route can also be stored as edge ids already?
+        candidate_edge_ids = {
+            graph.edge_indices_from_endpoints(u, v)[0] for u, v in zip(candidate_route[1:], candidate_route[:-1])
+        }
+        result_edge_ids = {
+            graph.edge_indices_from_endpoints(u, v)[0] for u, v in zip(result_route[1:], result_route[:-1])
+        }
+
+        shared_edges = candidate_edge_ids.intersection(result_edge_ids)
+
+        shared_edge_cost = sum([graph.get_edge_data_by_index(edge_id).length for edge_id in shared_edges])
+        result_edge_cost = sum([graph.get_edge_data_by_index(edge_id).length for edge_id in result_edge_ids])
+
+        return shared_edge_cost / result_edge_cost <= self.similarity_threshold
 
     @staticmethod
     def _convert_osm_route_to_linestring(graph: rx.PyGraph, route: rx.NodeIndices | list[int]) -> LineString:
